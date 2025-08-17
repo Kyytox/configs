6a88d3753e56b769bff8229fc748e0b62324c1f1
Kyytox <kytox.dev@protonmail.com>
2025-08-17 09:33:17 +0200

    upd temescope , add keys for snacks explorer

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,9 @@
 local tel_built = require("telescope.builtin")
 local ignore_filetypes_list = {
 	"venv",
-	"miniconda3",
-	"Youtube Music",
-	"__pycache__",
+	-- "miniconda3",
+	-- "Youtube Music",
+	-- "__pycache__",
 	"%.xlsx",
 	"%.jpg",
 	"%.png",
@@ -22,6 +22,9 @@ return {
 	config = function()
 		require("telescope").setup({
 			defaults = {
+				layout_config = {
+					vertical = { width = 0.5 },
+				},
 				file_ignore_patterns = ignore_filetypes_list,
 			},
 			pickers = {
@@ -31,10 +34,19 @@ return {
 					-- find_command = { "rg", "--files", "--glob", "!**/.git/*" },
 					no_ignore = true,
 				},
+				oldfiles = {
+					theme = "dropdown",
+				},
+				buffers = {
+					theme = "dropdown",
+				},
 			},
 		})
 
 		-- Keymaps
+		vim.keymap.set("n", "<leader>ff", tel_built.find_files, { desc = "Find Files" })
+		vim.keymap.set("n", "<leader>fg", tel_built.live_grep, { desc = "Live Grep" })
+		vim.keymap.set("n", "<leader>fb", tel_built.buffers, { desc = "Buffers" })
 		vim.keymap.set("n", "<leader>fh", tel_built.help_tags, { desc = "Help Tags" })
 	end,
 }
